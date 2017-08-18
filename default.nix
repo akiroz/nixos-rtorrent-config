452e0ebd739e5137a53b623c840376186de50b25
Akiroz <akiroz@users.noreply.github.com>
2017-08-18 17:00:59 +0800

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,17 +1,21 @@
 { ... }: let
+  globalEnable = false;
   mkRtorrentInstance = import ./rtorrent.nix;
 in {
+  users.extraUsers.rtorrent = {
+    isNormalUser = true;
+    extraGroups = [ "storage" ];
+  };
   imports = [
-    (mkRtorrentInstance 1)
-    (mkRtorrentInstance 2)
-    (mkRtorrentInstance 3)
-    (mkRtorrentInstance 4)
-    (mkRtorrentInstance 5)
-    (mkRtorrentInstance 6)
-    (mkRtorrentInstance 7)
-    (mkRtorrentInstance 8)
-    (mkRtorrentInstance 9)
-    (mkRtorrentInstance 10)
+    (mkRtorrentInstance { id = 1;   enable = globalEnable && true; })
+    (mkRtorrentInstance { id = 2;   enable = globalEnable && true; })
+    (mkRtorrentInstance { id = 3;   enable = globalEnable && true; })
+    (mkRtorrentInstance { id = 4;   enable = globalEnable && true; })
+    (mkRtorrentInstance { id = 5;   enable = globalEnable && true; })
+    (mkRtorrentInstance { id = 6;   enable = globalEnable && true; })
+    (mkRtorrentInstance { id = 7;   enable = globalEnable && true; })
+    (mkRtorrentInstance { id = 8;   enable = globalEnable && true; })
+    (mkRtorrentInstance { id = 9;   enable = globalEnable && true; })
+    (mkRtorrentInstance { id = 10;  enable = globalEnable && true; })
   ];
 }
-
